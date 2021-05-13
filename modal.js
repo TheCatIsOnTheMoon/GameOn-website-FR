0d99c7a5b473a47eb2b814a61c91642016001b2e
Sam Gillet-Garas <sgilletgaras@gmail.com>
2021-05-13 15:50:40 +0200

    add working close modal button

--- modal.js
+++ modal.js
@@ -10,12 +10,21 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector('.bground');
 const modalBtn = document.querySelectorAll('.modal-btn');
+const modalClose = document.querySelectorAll('.close');
 const formData = document.querySelectorAll('.formData');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
+// Close modal event
+modalClose.forEach((btn) => btn.addEventListener('click', closeModal));
+
 // launch modal form
 function launchModal() {
 	modalbg.style.display = 'block';
 }
+
+// close modal form
+function closeModal() {
+	modalbg.style.display = "none"
+  }
