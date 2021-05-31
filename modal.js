e39fd9dba4c81ae1782ec1c9cfef1271a121620e
Sam Gillet-Garas <sgilletgaras@gmail.com>
2021-05-31 21:07:26 +0200

    error message for required radio btn

--- modal.js
+++ modal.js
@@ -7,11 +7,18 @@ function editNav() {
 	}
 }
 
-// DOM Elements
-const modalbg = document.querySelector('.bground');
+// DOM Elements -------------------------------------------
+
+const modalBg = document.querySelector('.bground');
 const modalBtn = document.querySelectorAll('.modal-btn');
 const modalClose = document.querySelectorAll('.close');
 const formData = document.querySelectorAll('.formData');
+
+const locationRadioBtn = document.querySelectorAll('.checkbox-input');
+
+const submitBtn = document.querySelector('.btn-submit');
+
+// MODAL --------------------------------------------------
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
@@ -21,10 +28,88 @@ modalClose.forEach((btn) => btn.addEventListener('click', closeModal));
 
 // launch modal form
 function launchModal() {
-	modalbg.style.display = 'block';
+	modalBg.style.display = 'block';
 }
 
 // close modal form
 function closeModal() {
-	modalbg.style.display = "none";
+	modalBg.style.display = "none";
   }
+
+
+// FORM VALIDITY --------------------------------------------
+
+
+nameInput.addEventListener('input', () => {
+  nameInput.setCustomValidity('');
+  nameInput.checkValidity();
+});
+
+nameInput.addEventListener('invalid', () => {
+  if(nameInput.value === '') {
+    nameInput.setCustomValidity('Enter your username!');
+  } else {
+    nameInput.setCustomValidity('Usernames can only contain upper and lowercase letters. Try again!');
+  }
+});
+
+submitBtn.addEventListener('click', function (event) {
+	if (locationRadioBtn != "checked") {
+		document.getElementById('radio-error-message').innerHTML = 'Vous devez choisir une option.';
+	};
+})
+
+// TODO error message for required radio btn : 'Vous devez choisir une option.'
+
+// required radio button test 1
+
+// locationRadioBtn.onInvalid = function () {
+// 	this.setCustomValidity('Vous devez choisir une option.')
+// }
+
+// required radio button test 2
+// function valid() {
+// 	if (locationRadioBtn.not.checked) {
+// 		alert('Vous devez choisir une option.');
+// }
+// }
+
+// required radio button test 3
+// locationRadioBtn.addEventListener('invalid', function (event) {
+// 	if (event.target.validity.valueMissing) {
+// 	  event.target.setCustomValidity('Vous devez choisir une option.');
+// 	}
+//   })
+  
+//   submitBtn.addEventListener('change', function (event) {
+// 	event.target.setCustomValidity('');
+//   })
+
+// required radio button test 4
+// locationRadioBtn.addEventListener('input', () => {
+// 	locationRadioBtn.setCustomValidity('');
+// 	locationRadioBtn.checkValidity();
+//   });
+  
+//   locationRadioBtn.addEventListener('invalid', () => {
+// 	locationRadioBtn.setCustomValidity('Vous devez choisir une option.');
+// 	});
+
+// required radio button test 5
+
+submitBtn.addEventListener('click', function (event) {
+	if (locationRadioBtn != "checked") {
+		document.getElementById('radio-error-message').innerHTML = 'Vous devez choisir une option.';
+	};
+})
+
+// required radio button test 6
+
+// submitBtn.addEventListener('click', function (event) {
+// 	if (locationRadioBtn.target.validity.valueMissing) {
+// 		err.message= 'Vous devez choisir une option.';
+// 	};
+// })
+
+
+//   TODO submit btn becomes red when form is complete and valid, stay gry if not
