76563d0c1df1a5f279f37832d6b0570fa8884068
Samantha Gillet-Garas <sgilletgaras@gmail.com>
2021-06-06 18:51:45 +0200

    implement functional close modal btn

--- modal.js
+++ modal.js
@@ -10,6 +10,7 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalCloseBtn = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
@@ -18,4 +19,12 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal event
+modalCloseBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+
+// launch modal form
+function closeModal() {
+  modalbg.style.display = "none";
 }
